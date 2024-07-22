ecb81d13ab816890d10edd529ad7318407f49188
ifarbie <farizrifkyberliano@gmail.com>
2024-07-22 10:50:06 +0700

    initial commit: add project to github

---
+++ migrations/1721556545194_create-table-albums.js
@@ -0,0 +1,30 @@
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable('albums', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    name: {
+      type: 'VARCHAR(100)',
+      notNull: true,
+    },
+    year: {
+      type: 'INTEGER',
+      notNull: true,
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable('albums');
+};
